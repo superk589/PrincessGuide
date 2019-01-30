511a0fde3662274755604a84b15799f1fcdff0c0
superk <superk589@gmail.com>
2019-01-30 14:57:02 +0800

    update skill detail

--- PrincessGuide/Model/Action/MoveAction.swift
+++ PrincessGuide/Model/Action/MoveAction.swift
@@ -18,6 +18,7 @@ class MoveAction: ActionParameter {
         case absolute
         case targetByVelocity
         case absoluteByVelocity
+        case absoluteWithoutDirection
     }
     
     var moveType: MoveType {
@@ -25,50 +26,48 @@ class MoveAction: ActionParameter {
     }
     
     override func localizedDetail(of level: Int, property: Property = .zero, style: CDSettingsViewController.Setting.ExpressionStyle = CDSettingsViewController.Setting.default.expressionStyle) -> String {
-//        switch moveType {
-//        case .targetReturn:
-//            let format = NSLocalizedString("Change self position to %@ then return.", comment: "")
-//            return String(format: format, targetParameter.buildTargetClause())
-//        case .absoluteReturn:
-//            if actionValue1 > 0 {
-//                let format = NSLocalizedString("Change self position %@ forward then return.", comment: "")
-//                return String(format: format, actionValue1.description)
-//            } else {
-//                let format = NSLocalizedString("Change self position %@ backward then return.", comment: "")
-//                return String(format: format, (-actionValue1).description)
-//            }
-//        case .target:
-//            let format = NSLocalizedString("Change self position to %@.", comment: "")
-//            return String(format: format, targetParameter.buildTargetClause())
-//        case .absolute:
-//            if actionValue1 > 0 {
-//                let format = NSLocalizedString("Change self position %@ forward.", comment: "")
-//                return String(format: format, actionValue1.description)
-//            } else {
-//                let format = NSLocalizedString("Change self position %@ backward.", comment: "")
-//                return String(format: format, (-actionValue1).description)
-//            }
-//        case .targetByVelocity:
-//            if actionValue1 > 0 {
-//                let format = NSLocalizedString("Move to %@ in front of %@ with velocity %@/s.", comment: "")
-//                return String(format: format, actionValue1.description, targetParameter.buildTargetClause(), actionValue2.description)
-//            } else {
-//                let format = NSLocalizedString("Move to %@ behind of %@ with velocity %@/s.", comment: "")
-//                return String(format: format, (-actionValue1).description, targetParameter.buildTargetClause(), actionValue2.description)
-//            }
-//        case .absoluteByVelocity:
-//            if actionValue1 > 0 {
-//                let format = NSLocalizedString("Move forward %@ with velocity %@/s.", comment: "")
-//                return String(format: format, actionValue1.description, actionValue2.description)
-//            } else {
-//                let format = NSLocalizedString("Move backward %@ with velocity %@/s.", comment: "")
-//                return String(format: format, (-actionValue1).description, actionValue2.description)
-//            }
-//        default:
-//            return super.localizedDetail(of: level, property: property, style: style)
-//        }
-        let format = NSLocalizedString("Change self position.", comment: "")
-        return String(format: format)
+        switch moveType {
+        case .targetReturn:
+            let format = NSLocalizedString("Change self position to %@ then return.", comment: "")
+            return String(format: format, targetParameter.buildTargetClause())
+        case .absoluteReturn:
+            if actionValue1 > 0 {
+                let format = NSLocalizedString("Change self position %@ forward then return.", comment: "")
+                return String(format: format, actionValue1.roundedValueString(.down))
+            } else {
+                let format = NSLocalizedString("Change self position %@ backward then return.", comment: "")
+                return String(format: format, (-actionValue1).roundedValueString(.down))
+            }
+        case .target:
+            let format = NSLocalizedString("Change self position to %@.", comment: "")
+            return String(format: format, targetParameter.buildTargetClause())
+        case .absolute, .absoluteWithoutDirection:
+            if actionValue1 > 0 {
+                let format = NSLocalizedString("Change self position %@ forward.", comment: "")
+                return String(format: format, actionValue1.roundedValueString(.down))
+            } else {
+                let format = NSLocalizedString("Change self position %@ backward.", comment: "")
+                return String(format: format, (-actionValue1).roundedValueString(.down))
+            }
+        case .targetByVelocity:
+            if actionValue1 > 0 {
+                let format = NSLocalizedString("Move to %@ in front of %@ with velocity %@/s.", comment: "")
+                return String(format: format, actionValue1.roundedValueString(.down), targetParameter.buildTargetClause(), actionValue2.roundedValueString(.down).description)
+            } else {
+                let format = NSLocalizedString("Move to %@ behind of %@ with velocity %@/s.", comment: "")
+                return String(format: format, (-actionValue1).roundedValueString(.down), targetParameter.buildTargetClause(), actionValue2.roundedValueString(.down).description)
+            }
+        case .absoluteByVelocity:
+            if actionValue1 > 0 {
+                let format = NSLocalizedString("Move forward %@ with velocity %@/s.", comment: "")
+                return String(format: format, actionValue1.roundedValueString(.down), actionValue2.description)
+            } else {
+                let format = NSLocalizedString("Move backward %@ with velocity %@/s.", comment: "")
+                return String(format: format, (-actionValue1).roundedValueString(.down), actionValue2.description)
+            }
+        default:
+            return super.localizedDetail(of: level, property: property, style: style)
+        }
     }
     
 }
