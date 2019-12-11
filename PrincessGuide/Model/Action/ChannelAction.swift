8622cf6d4517e9c809ac76f269eb43fd8c61e312
superk <superk589@gmail.com>
2019-12-11 13:04:34 +0800

    Fix skill description

--- PrincessGuide/Model/Action/ChannelAction.swift
+++ PrincessGuide/Model/Action/ChannelAction.swift
@@ -16,14 +16,23 @@ class ChannelAction: AuraAction {
     }
     
     var releaseType: ReleaseType {
-        return ReleaseType(rawValue: actionDetail3) ?? .unknown
+        return ReleaseType(rawValue: actionDetail2) ?? .unknown
     }
     
     override func localizedDetail(of level: Int, property: Property, style: CDSettingsViewController.Setting.ExpressionStyle) -> String {
         switch releaseType {
         case .damage:
-            let format = NSLocalizedString("Channel for [%@]s, interruptible by receiving damange, %@ %@ [%@]%@ %@.", comment: "")
-            return String(format: format, buildExpression(of: level, actionValues: durationValues, roundingRule: nil, style: style, property: property), auraActionType.description, targetParameter.buildTargetClause(), buildExpression(of: level, roundingRule: .awayFromZero, style: style, property: property), percentModifier.description, auraType.description)
+            let format = NSLocalizedString("Channel for [%@]s, interruptible by receiving damange %d times, %@ %@ [%@]%@ %@.", comment: "")
+            return String(
+                format: format,
+                buildExpression(of: level, actionValues: durationValues, roundingRule: nil, style: style, property: property),
+                actionDetail3,
+                auraActionType.description,
+                targetParameter.buildTargetClause(),
+                buildExpression(of: level, roundingRule: .awayFromZero, style: style, property: property),
+                percentModifier.description,
+                auraType.description
+            )
         default:
             return super.localizedDetail(of: level, property: property, style: style)
         }
