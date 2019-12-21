3805ed3f1a0798aace84aa3c6503c600eb3fe8e8
superk <superk589@gmail.com>
2019-12-21 22:48:44 +0800

    Minor update

--- PrincessGuide/Model/Action/RatioDamageAction.swift
+++ PrincessGuide/Model/Action/RatioDamageAction.swift
@@ -17,7 +17,26 @@ class RatioDamageAction: ActionParameter {
     }
     
     override func localizedDetail(of level: Int, property: Property = .zero, style: CDSettingsViewController.Setting.ExpressionStyle = CDSettingsViewController.Setting.default.expressionStyle) -> String {
-        let format = NSLocalizedString("Deal damage equal to [%@]%% of target's max HP to %@.", comment: "")
-        return String(format: format, buildExpression(of: level, roundingRule: .none, style: style, property: property), targetParameter.buildTargetClause())
+        
+        switch hpType {
+        case .max:
+            let format = NSLocalizedString("Deal damage equal to [%@]%% of target's max HP to %@.", comment: "")
+            return String(format: format, buildExpression(of: level, roundingRule: .none, style: style, property: property), targetParameter.buildTargetClause())
+        case .current:
+            let format = NSLocalizedString("Deal damage equal to [%@]%% of target's current HP to %@.", comment: "")
+            return String(format: format, buildExpression(of: level, roundingRule: .none, style: style, property: property), targetParameter.buildTargetClause())
+        default:
+            return super.localizedDetail(of: level, property: property, style: style)
+        }
+    }
+    
+    var hpType: HPType {
+        return HPType(rawValue: actionDetail1) ?? .unknown
+    }
+    
+    enum HPType: Int {
+        case unknown = 0
+        case max = 1
+        case current
     }
 }
